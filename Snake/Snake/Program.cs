a4d1ee333e44db631b4c40c2e2f58867002ec0e6
TimOn <via-lactea757@yandex.ru>
2021-11-10 12:37:07 +0500

    C# lesson 3

---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
+            /*int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+            Draw(x1, y1, sym1);*/
+            /* Draw(p1.x, p1.y, p1.sym);*/
+
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            p1.Draw();
+            /* int x2 = 4;
+             int y2 = 5;
+             char sym2 = '#';
+             Draw(x2, y2, sym2);*/
+            /*Draw(p2.x, p2.y, p2.sym);*/
+
+            Console.ReadLine();
+        }
+        /*static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }*/
+    }
+}
